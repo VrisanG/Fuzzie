9d212a8b0a283c682c9b2bec6e20783fb4340627
Vrisan Gaur <gaurvrian@gmail.com>
2024-09-30 19:12:41 +0530

    Initial commit

---
+++ src/components/global/Navbar.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import React from "react";
+import { MdOutlineMenu } from "react-icons/md";
+
+const Navbar = () => {
+  let user: boolean = true;
+  return (
+    <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+      <aside className="flex items-center">
+        <p className="text-3xl font-bold">Fu</p>
+        <img
+          src="https://github.com/webprodigies/fuzzie-production/blob/main/public/fuzzieLogo.png?raw=true"
+          width={15}
+          height={15}
+          className="shadow-lg"
+          alt="Fuzzie Logo"
+        />
+        <p className="text-3xl font-bold">zie</p>
+      </aside>
+      <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
+        <ul className="flex items-center gap-4 list-none">
+          <li>
+            <Link href="">Products</Link>
+          </li>
+          <li>
+            <Link href="">Pricing</Link>
+          </li>
+          <li>
+            <Link href="">Clients</Link>
+          </li>
+          <li>
+            <Link href="">Resources</Link>
+          </li>
+          <li>
+            <Link href="">Documentation</Link>
+          </li>
+          <li>
+            <Link href="">Enterprise</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <aside>
+        <div className="flex items-center">
+          <Link
+            href="/dashboard"
+            className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          >
+            <span className="absolute inset-[-100%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]"></span>
+            <span className="inline-flex h-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              {user ? "DashBoard" : "Get Started"}
+            </span>
+          </Link>
+          <MdOutlineMenu className="md:hidden size-7 translate-y-2" />
+        </div>
+      </aside>
+      
+    </header>
+  );
+};
+
+export default Navbar;
